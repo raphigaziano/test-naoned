5a6aab699337480a07cc775364483963fdc5510f
raphigaziano <r.gaziano@gmail.com>
2013-07-17 02:06:38 +0200

    models first draft

---
+++ models/basemodel.php
@@ -0,0 +1,53 @@
+<?php
+
+include('../constants.php');
+
+/**
+ * Base model class, handling basic db connection
+ **/
+abstract Class Model {
+
+    protected static $db;
+    protected $queries = array();
+
+    /**
+     * Instance constructor.
+     **/
+    public function __construct() {
+        $this->_initQueries();
+    }
+
+    /**
+     * Database Initialisation.
+     *
+     * @return void
+     **/
+    public static function initDb() {
+        try {
+            self::$db = new PDO(DB_CONNECTION_STRING, DB_USER, DB_PSWD);
+        } catch (PDOError $e) {
+            die('Impossible de se connecter a la bdd: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Helper for subclasses - prepare the passed SQL statement with
+     * PDO.
+     *
+     * @param $sqlstmnt: SQL statement to store.
+     * @return prepared statement.
+     **/
+    protected function _prepareRequest($sqlstmnt) {
+        return self::$db->prepare($sqlstmnt);
+    }
+
+    /**
+     * Initialize internal SQL queries.
+     * Subclasses must override this to define their own SQL queries.
+     **/
+    abstract protected function _initQueries();
+}
+
+Model::initDb();
+
+?>
---
+++ models/categories.php
@@ -0,0 +1,68 @@
+<?php
+include('basemodel.php');
+
+/**
+ * Model class for Category objects
+ **/
+Class Categorie extends Model {
+
+    private $id, $label, $parent;
+
+    /**
+     * Object's hydratation from from a DB row.
+     *
+     * @param $args: Array of fields from the fetched row.
+     * @return void
+     **/
+    protected function _initFromDb($args) {
+        $this->id     = $args['cat_id'];
+        $this->label  = $args['cat_label'];
+        $this->parent = $args['cat_parent'];
+    }
+
+    /**
+     * Initialize internal SQL queries.
+     *
+     * @return void
+     **/
+    protected function _initQueries() {
+        $this->queries['getById'] = $this->_prepareRequest(
+            "SELECT * FROM categorie WHERE cat_id = :id;"
+        );
+    }
+
+    /**
+     * Alternate, static constructor.
+     *
+     * @param $id: Primary key of the requested row.
+     * @return New instance of Categorie from fethed db entry
+     **/
+    public static function getById($id) {
+        $obj = new Categorie();
+        $request = $obj->queries['getById'];
+        $request->execute(array(':id' => $id));        
+	$db_fields = $request->fetch();	
+	if ($db_fields) {
+            $obj->_initFromDb($db_fields);
+            return $obj;
+        } else {
+            // TODO: Better Error Handling
+	       return NULL;
+        }
+    }
+}
+
+// TESTING
+//$c = Categorie::getById('1');
+echo '<pre>';
+echo "simple instanciation</br>";
+$c = new Categorie();
+var_dump($c);
+echo '</br></br>';
+echo "intanciate from static</br>";
+var_dump(Categorie::getById(1));
+echo '</br></br>';
+var_dump(Categorie::getById(2));
+echo 'done';
+echo '</pre>';
+?>
