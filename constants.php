38be8f95af41b95c0474f3e432d3bd07fabb0c4c
raphigaziano <r.gaziano@gmail.com>
2013-07-17 10:53:51 +0200

    removed uneccesary constants + added static url shortcut

--- constants.php
+++ constants.php
@@ -7,14 +7,21 @@
 // Constants Definitions
 // ---------------------
 
-// Site pathes
-define('SITE_ROOT', realpath(dirname(__FILE__)));
-define('STATIC_ROOT', SITE_ROOT . '/static');
-
 // Db settings
 define('DB_CONNECTION_STRING', 'mysql:dbname=naoned_test;host=127.0.0.1');
 define('DB_USER', 'naoned_test');
 define('DB_PSWD', NULL);
 
+// Shortcut, functions...
+/**
+ * Echoes the url to the the specified ressource, by prefixing it with 
+ * '/static/'.
+ * 
+ * @param $ressource: path to ressource, relative to site_root/static/
+ * @return void
+ */
+function get_static_url($ressource) {
+    echo "/static/$ressource";
+}
 ?>
 
