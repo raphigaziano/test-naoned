039509bd52bba6db61e162613aaaeb929e9571ce
raphigaziano <r.gaziano@gmail.com>
2013-07-17 10:54:07 +0200

    bootstrap test

--- views/header.php
+++ views/header.php
@@ -4,8 +4,35 @@
         <meta charset='utf-8'/>
         <title>Test Naoned - Annuaire Musées</title>
         <link rel='stylesheet' type='text/css'
-              href=<?php STATIC_ROOT . 'bootstrap/boostrap.min.css' ?> />
+              href='<?php get_static_url('bootstrap/css/bootstrap.min.css'); ?>' />
+ 	<link rel='stylesheet' type='text/css'
+	      href='<?php get_static_url('bootstrap-responsive.min.css'); ?>' />
     </head>
 
     <body>
-    <!-- header, nav... -->
+    <!-- header -->
+    <header>
+      <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-inner">
+          <div class="container-fluid">
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="brand" href="#">Project name</a>
+            <div class="nav-collapse collapse">
+              <p class="navbar-text pull-right">
+                Logged in as <a href="#" class="navbar-link">Username</a>
+              </p>
+              <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </div><!--/.nav-collapse -->
+          </div>
+        </div>
+      </div>
+
+    </header>
