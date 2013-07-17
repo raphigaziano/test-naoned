a1b4984cad548736dc64857c812bef7baccda60a
raphigaziano <r.gaziano@gmail.com>
2013-07-17 10:17:42 +0200

    basic views layout

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+include('constants.php'); 
+include('views/header.php');
+
+?><p>
+    content content
+    lorem lala
+    </p><?php
+
+include('views/footer.php');
+?>
---
+++ views/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta content='text/html' charset='utf-8'/>
+        <title>Test Naoned - Annuaire Musées</title>
+        <link rel='stylesheet' type='text/css'
+              href=<?php STATIC_ROOT . 'bootstrap/boostrap.min.css' ?> />
+    </head>
+
+    <body>
+    <!-- header, nav... -->
