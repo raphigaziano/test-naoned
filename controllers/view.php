5691d97c8397f39ca55b55d227230317ef74fe0d
raphigaziano <r.gaziano@gmail.com>
2013-07-18 09:55:58 +0200

    moved fiches diplay into separate view files

--- controllers/view.php
+++ controllers/view.php
@@ -5,19 +5,8 @@
 // View single fiche
 if (isset($_GET['post'])) {
     $f = Fiche::getById($_GET['post']);
-    // TODO: move into view
-?>
-    <h1><?php echo $f->getLabel(); ?></h1>
-    <p><?php echo $f->getDescription(); ?></p>
-    <p>
-        Categories: 
-        <?php foreach ($f->getCategories() as $c): ?>
-            <a href="?cat=<?php echo $c->getId(); ?>">
-              <?php echo $c->getLabel(); ?>
-            </a>
-        <?php endforeach; ?>
-    </p>
-<?php } 
+    include('views/singlepost.php');
+} 
 // List fiches
 else {
     // Category filter
@@ -35,16 +24,5 @@ else {
     if (isset($c)) {
         echo 'Catégorie: ' . $c->getLabel() . '<br/>';
     }
-    // TODO: move into view
-    echo count($fiches) . ' Fiches sont affichées.';
-    foreach ($fiches as $f) {
-        ?>
-        <p>
-            <h4>
-              <a href="?action=view&post=<?php echo $f->getId();?>">
-                <?php echo $f->getLabel(); ?>
-              </a>
-            </h4>
-        </p>
-    <?php }
+    include('views/fichelist.php');
 }
---
+++ views/singlepost.php
@@ -0,0 +1,11 @@
+    <h1><?php echo $f->getLabel(); ?></h1>
+    <p><?php echo $f->getDescription(); ?></p>
+    <p>
+        Categories: 
+        <?php foreach ($f->getCategories() as $c): ?>
+            <a href="?cat=<?php echo $c->getId(); ?>">
+              <?php echo $c->getLabel(); ?>
+            </a>
+        <?php endforeach; ?>
+    </p>
+
