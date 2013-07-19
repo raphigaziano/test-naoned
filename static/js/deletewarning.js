fd6f60a993cb0c8a503bcbf00c518b697e7b725f
raphigaziano <r.gaziano@gmail.com>
2013-07-19 20:24:58 +0200

    refacto warning js in a function

--- static/js/deletewarning.js
+++ static/js/deletewarning.js
@@ -2,17 +2,15 @@
  * Warning alert when a "delete" button is pressed
  *
  */
+function warnOnDelete(elem) {
+    var msg = "Etes vous sur de vouloir supprimer cette entrée?";
+    if ( $(elem).parents('form').hasClass('item-edit') ) {
+        msg += "\nSa supression entrainera celle de toutes les "
+                + "sous-catégories ou fiches associé(e)s!";
+    }
+    if (confirm(msg)) {
+        return true;
+    }
+    return false;
+}
 
-(function() {
-    $('button[name=delete]').click(function(ev) {
-        var msg = "Etes vous sur de vouloir supprimer cette entrée?";
-        if ( $(this).parents('form').hasClass('categorie-edit') ) {
-            msg += "\nSa supression entrainera celle de toutes les "
-                    + "sous-catégories ou fiches associé(e)s!";
-        }
-        if (confirm(msg)) {
-            return true;
-        }
-        ev.preventDefault();
-    });
-})();
