884a2be75dac9004144a1b839cd590b00f83835f
raphigaziano <r.gaziano@gmail.com>
2013-07-19 14:03:59 +0200

    fiche post handler

--- controllers/fiche-editcontroller.php
+++ controllers/fiche-editcontroller.php
@@ -1,10 +1,11 @@
 <?php
 
-Class FicheEditController extends BaseController {
+Class FicheEditController extends EditController {
 
     private $fiche;
 
     public function __construct() {
+        $this->modelClass = 'Fiche';
         if (isset($_REQUEST['fiche'])) {
             $this->fiche = Fiche::getById($_REQUEST['fiche']);
         } else {
@@ -18,6 +19,17 @@ Class FicheEditController extends BaseController {
         $t = new MainTemplate(array('forms/fiche'));
         $t->render(array('f'    => $this->fiche,
                          'cats' => $cats));
+    }
+
+    public function doPost() {
+        $args = array(
+            'fi_id'          => $_POST['id'],
+            'fi_label'       => $_POST['label'],
+            'fi_description' => $_POST['description'],
+            'cat_id'         => $_POST['cat'],
+
+        );
+        $this->dispatchAction($args);
     }
 }
 
